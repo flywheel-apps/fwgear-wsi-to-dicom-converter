45707e1931c415d374d6bc068727daacf26e76eb
davidparker1 <davidparker@flywheel.io>
2020-06-30 15:18:32 -0400

    Added failure logic to the python script

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 from pathlib import Path
 import subprocess as sp
 import sys
@@ -60,7 +61,13 @@ def setup(context):
         series_description = context.config.get('seriesDescription')
     
     output_dir = context.work_dir/Path(series_description)
-    command.extend('--outFolder', output_dir)
+    
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+        
+    command.extend(['--outFolder', output_dir])
+    # Stringify all PATHS in the command for logging stuff.
+    command = [str(c) for c in command]
     
     context.log.info("Command to call: \n" + " ".join(command))
     
@@ -73,35 +80,57 @@ def run(command):
     pr = sp.Popen(command, shell=False)
     pr.wait()
     rc = pr.returncode
-    
     if rc != 0:
         context.log.error('Error running WIS')
     return(rc)
 
 def cleanup(context, output_dir):
+    # Changed from analysis to converter
+    # zipped_output = context.output_dir/Path(f'{output_dir}.zip')
+    # zip_tools.zip_output(context.work_dir, zipped_output)
     
-    zipped_output = context.output_dir/Path(f'{output_dir}.zip')
-    zip_tools.zip_output(context.work_dir, zipped_output)
+    dicom_directory = context.work_dir/output_dir
     
+    src_files = os.listdir(dicom_directory)
+    
+    if len(src_files) == 0:
+        log.error('No files generated.  Assuming error')
+        raise Exception('No output Files Generated')
+    
+    for file_name in src_files:
+        full_file_name = os.path.join(dicom_directory, file_name)
+        if os.path.isfile(full_file_name):
+            dest = context.output_dir/Path(file_name)
+            os.rename(full_file_name, dest)
+            
 
 
 if __name__ == '__main__':
     # Set return code to zero optimistically
     rc = 0
     with flywheel_gear_toolkit.GearToolkitContext() as context:
+        try:
+            context.init_logging()
+            context.log_config()
+            print(os.listdir('/flywheel/v0'))
+            
+            valid = fail_check(context)
+            context.log.info(f'Input file is valid:   {valid}')
+            
+            command, series_description = setup(context)
+            
+            rc = run(command)
+            # return code currently unreliable
+            context.log.info(f'Exit Code: {rc}')
+            
+            cleanup(context, series_description)
+            
+        except Exception as e:
+            context.log.error('Error running gear')
+            context.log.exception(e)
+            sys.exit(1)
         
-        context.init_logging()
-        context.log_config()
-        
-        valid = fail_check(context)
-        context.log.info(f'Input file is valid:   {valid}')
-        
-        command, series_description = setup(context)
-        
-        rc = run(command)
-        
-        cleanup(context, series_description)
-        
-        sys.exit(rc)
+    
+    sys.exit(0)
 
 
